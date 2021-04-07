c082719788cfe38390cf98269ad848b8e149d5dc
Azeet Chebrolu <azeet.chebrolu@aa.com>
2021-04-07 09:41:55 -0700

    fix proxy

--- tot-look-ahead/index.ts
+++ tot-look-ahead/index.ts
@@ -1,43 +1,41 @@
 import * as tl from 'azure-pipelines-task-lib/task';
 import fetch from 'node-fetch';
-import FormData = require("form-data");
-import {getWorkItemsforNotes, getReleaseEndTime , getReleaseStartTime} from './workitem';
+import FormData = require('form-data');
+import { getWorkItemsforNotes, getReleaseEndTime, getReleaseStartTime } from './workitem';
 
-const httpsProxyAgent = require('https-proxy-agent');
+import httpsProxyAgent from 'https-proxy-agent';
 
-    const url = 'https://spteam.aa.com/sites/MnE/TechOps';
-    const listName = 'TOT-LookAhead';
-    const listNameType = 'SP.Data.TOTLookAheadListItem';
-    let clientId: string | undefined = tl.getInput('clientId', false);    
-    let clientSecret: string | undefined = tl.getInput('clientSecret', false);
-    let changeNo: string | undefined = tl.getInput('changeNo', false);
-    let status: string | undefined = tl.getInput('status', false);
-    let changeTitle: string | undefined =  tl.getInput('changeTitle', false);
-    let application: string | undefined = tl.getInput('application', false);
-    let businessDescription: string | undefined = tl.getInput('businessDescription', false) 
-    let technicalDescription: string | undefined = tl.getInput('technicalDescription', false);
-    let impact: string | undefined = tl.getInput('impact', false);
-    let srManager: string | undefined = tl.getInput('srManager', false);
-    let startDate: string | undefined|null = ''; 
-    let endDate: string | undefined|null = ''; 
-    let teamsInvolved: string | undefined = tl.getInput('teamsInvolved', false);
-    let comms: string | undefined = tl.getInput('comms', false);
-    let commsUrl: string | undefined = tl.getInput('commsUrl', false);
-    let commsText: string | undefined = tl.getInput('commsText', false);
-    let fleetMigrationImpact: string | undefined = tl.getInput('fleetMigrationImpact', false);
-    let assignedResource: string | undefined = tl.getInput('assignedResource', false);
-    let additionalNotesUrl: string | undefined = tl.getInput('additionalNotesUrl', false);
-    let additionalNotesText: string | undefined = tl.getInput('additionalNotesText', false);
-    let proxyUrl: string | undefined = tl.getInput('proxyUrl', false);
-
+const url = 'https://spteam.aa.com/sites/MnE/TechOps';
+const listName = 'TOT-LookAhead';
+const listNameType = 'SP.Data.TOTLookAheadListItem';
+let clientId: string | undefined = tl.getInput('clientId', false);
+let clientSecret: string | undefined = tl.getInput('clientSecret', false);
+let changeNo: string | undefined = tl.getInput('changeNo', false);
+let status: string | undefined = tl.getInput('status', false);
+let changeTitle: string | undefined = tl.getInput('changeTitle', false);
+let application: string | undefined = tl.getInput('application', false);
+let businessDescription: string | undefined = tl.getInput('businessDescription', false);
+let technicalDescription: string | undefined = tl.getInput('technicalDescription', false);
+let impact: string | undefined = tl.getInput('impact', false);
+let srManager: string | undefined = tl.getInput('srManager', false);
+let startDate: string | undefined | null = '';
+let endDate: string | undefined | null = '';
+let teamsInvolved: string | undefined = tl.getInput('teamsInvolved', false);
+let comms: string | undefined = tl.getInput('comms', false);
+let commsUrl: string | undefined = tl.getInput('commsUrl', false);
+let commsText: string | undefined = tl.getInput('commsText', false);
+let fleetMigrationImpact: string | undefined = tl.getInput('fleetMigrationImpact', false);
+let assignedResource: string | undefined = tl.getInput('assignedResource', false);
+let additionalNotesUrl: string | undefined = tl.getInput('additionalNotesUrl', false);
+let additionalNotesText: string | undefined = tl.getInput('additionalNotesText', false);
+let proxyUrl: string | undefined = tl.getInput('proxyUrl', false);
 
 async function run() {
   try {
-    
-    startDate ='2021-03-03T21:21:54Z'//await getReleaseStartTime() || (new Date).toLocaleString()
-    endDate = '2021-03-04T21:21:54Z'//await getReleaseEndTime() || (new Date).toLocaleString()
-    businessDescription =(businessDescription||'')+ '\n'// + (await getWorkItemsforNotes());
-    status = status == "Succeeded" ? "Complete" : status;
+    startDate = '2021-03-03T21:21:54Z'; //await getReleaseStartTime() || (new Date).toLocaleString()
+    endDate = '2021-03-04T21:21:54Z'; //await getReleaseEndTime() || (new Date).toLocaleString()
+    businessDescription = (businessDescription || '') + '\n'; // + (await getWorkItemsforNotes());
+    status = status == 'Succeeded' ? 'Complete' : status;
     const tenantName = 'spteam.aa.com';
     const tenantId = '49793faf-eb3f-4d99-a0cf-aef7cce79dc1';
     const resourceId = '00000003-0000-0ff1-ce00-000000000000';
@@ -48,26 +46,26 @@ async function run() {
     tokenBody.append('client_id', `${clientId}@${tenantId}`);
     tokenBody.append('client_secret', clientSecret);
     tokenBody.append('resource', `${resourceId}/${tenantName}@${tenantId}`);
- ;
     //1. Get Access Token
-    let response:any=''
-    if  (proxyUrl){
-     const agent = new httpsProxyAgent(proxyUrl)
-      console.log('proxyUrl' +proxyUrl)
-     response = await fetch(tokenUrl, {
-      method: 'POST',
-      agent: agent,
-      body: tokenBody,
-    }).then((res) => res.json());
-  } else {
-    console.log('non proxyUrl')
-    response = await fetch(tokenUrl, {
-      method: 'POST',      
-      body: tokenBody,
-    }).then((res) => res.json());
-  }
+    let response: any = '';
+    if (proxyUrl) {
+      const agent = httpsProxyAgent(proxyUrl);
+      console.log('proxyUrl' + proxyUrl);
+      response = await fetch(tokenUrl, {
+        method: 'POST',
+        agent: agent,
+        body: tokenBody,
+      }).then((res) => res.json());
+    } else {
+      console.log('non proxyUrl');
+      response = await fetch(tokenUrl, {
+        method: 'POST',
+        body: tokenBody,
+      }).then((res) => res.json());
+    }
     const access_token = response.access_token;
     //console.log('Access Token:', response);
+    //return;
     //2. Get Digest
     const digestTokenHeaders = {
       Authorization: `Bearer ${access_token}`,
@@ -80,7 +78,7 @@ async function run() {
     }).then((res) => res.json());
     //console.log('digestResponse', digestResponse);
     const digest = digestResponse.d.GetContextWebInformation.FormDigestValue;
-    //3.Create TOT LookAhead Item    
+    //3.Create TOT LookAhead Item
     const lookAheadItemBody = {
       __metadata: {
         type: listNameType,
@@ -111,7 +109,7 @@ async function run() {
         Url: additionalNotesText || additionalNotesUrl,
       },
       Fleet_x0020_Migration_x0020_Cale: false,
-     };
+    };
     const listUrl = `${url}/_api/web/lists/GetByTitle('${listName}')/items`;
     const lookAheadItemHeaders = {
       Authorization: `Bearer ${access_token}`,
@@ -119,17 +117,17 @@ async function run() {
       'Content-Type': 'application/json;odata=verbose',
 
       'X-RequestDigest': digest,
-    };    
+    };
     const itemResponse = await fetch(listUrl, {
       method: 'POST',
       body: JSON.stringify(lookAheadItemBody),
       headers: lookAheadItemHeaders,
     }).then((res) => {
       console.log(res.clone().status);
-      console.log(res.clone().statusText);        
+      console.log(res.clone().statusText);
       return res.json();
     });
-    
+
     console.log('Successfully added item to TOT LookAhead List');
     console.log('List Item Link:', itemResponse.d.__metadata.uri);
     tl.setVariable('TOT_LookAhead_Item_Link', itemResponse.d.__metadata.uri, false, true);
@@ -140,25 +138,23 @@ async function run() {
 enableDebugValues();
 run();
 
-function enableDebugValues(){
-  proxyUrl='http://inetgw.aa.com:9093'
-  clientId='38ca5562-5dcf-4f7a-9afa-75cd3f7d551b'
-  clientSecret= '6y2Zce-15M-.48cfNif.2LMC.CrVKTsj1C'
-  changeNo='testing-TOT'
-  status='Completed'
-  changeTitle='testing the tool'
-  application='LMO'
-  businessDescription =''
-  technicalDescription =''
-  impact ="No Outage Expected",
-  srManager ='Kyle Wander'
-  teamsInvolved ='LM-Apps'
-  comms =''
-  commsUrl =''
-  commsText =''
-  fleetMigrationImpact ='No'
-  assignedResource ='No'
-  additionalNotesUrl =''
-  additionalNotesText =''
-
+function enableDebugValues() {
+  proxyUrl = 'http://inetgw.aa.com:9093';
+  clientId = '38ca5562-5dcf-4f7a-9afa-75cd3f7d551b';
+  clientSecret = '6y2Zce-15M-.48cfNif.2LMC.CrVKTsj1C';
+  changeNo = 'testing-TOT';
+  status = 'Completed';
+  changeTitle = 'testing the tool';
+  application = 'LMO';
+  businessDescription = '';
+  technicalDescription = '';
+  (impact = 'No Outage Expected'), (srManager = 'Kyle Wander');
+  teamsInvolved = 'LM-Apps';
+  comms = '';
+  commsUrl = '';
+  commsText = '';
+  fleetMigrationImpact = 'No';
+  assignedResource = 'No';
+  additionalNotesUrl = '';
+  additionalNotesText = '';
 }
